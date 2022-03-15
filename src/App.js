0b12c51cb160256b1d548ce0efbd315b5c458481
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-15 10:42:17 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css'
+import Box from './Box'
 
 const App=()=>{
   return (
     <div className="root">
            <h1>First React App</h1>
+           <hr/>
+           <Box/>
+           <Box/>
+           <Box/>
+           <Box/>
     </div>
   )
 }
